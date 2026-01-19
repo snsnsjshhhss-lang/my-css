9391a8930a7e696970756f9e640ab494c57277b3
snsnsjshhhss-lang <snsnsjshhhss@gmail.com>
2026-01-19 06:41:10 +0200

    Refactor no-zoom.js to use DOMContentLoaded
    Updated the script to use DOMContentLoaded event for initialization.

--- css/js/no-zoom.js
+++ css/js/no-zoom.js
@@ -1,13 +1,11 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-    // منع التكبير بالـ Pinch
     document.addEventListener('touchstart', function (e) {
         if (e.touches.length > 1) {
             e.preventDefault();
         }
     }, { passive: false });
 
-    // منع التكبير بالـ Double Tap
     let lastTouchEnd = 0;
     document.addEventListener('touchend', function (e) {
         const now = Date.now();
@@ -17,4 +15,4 @@
         lastTouchEnd = now;
     }, false);
 
-})();
+});
